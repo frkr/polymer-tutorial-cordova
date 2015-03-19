9efa47b5586e9d38eeb13980027e819184b2caef
Davi <frkr@users.noreply.github.com>
2015-03-19 16:13:04 -0300

    show only usuful commands

--- create.bat
+++ create.bat
@@ -1,13 +1,14 @@
 call cordova create polymer-tutorial-cordova org.polymer.cordova Polymer
-cd polymer-tutorial-cordova
-echo PUT THIS ON INDEX=finished/index.html >> config.xml
-rmdir /S /Q www
+@cd polymer-tutorial-cordova
+echo ON config.xml, 'INDEX=' PUT THIS:: INDEX=finished/index.html >> config.xml
+@rmdir /S /Q www
 git clone -b 0.5.4-take2 https://github.com/Polymer/polymer-tutorial.git www
-cd www
-rmdir /S /Q .git
-rmdir /S /Q starter
-rmdir /S /Q step-1
-rmdir /S /Q step-2
-rmdir /S /Q step-3
-del .gitignore
-del README.md
+@cd www
+@rmdir /S /Q .git
+@rmdir /S /Q starter
+@rmdir /S /Q step-1
+@rmdir /S /Q step-2
+@rmdir /S /Q step-3
+@del .gitignore
+@del README.md
+@echo ready!?
