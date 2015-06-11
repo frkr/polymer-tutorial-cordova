cc0afae9a9c56f63594831c491e507510840b8d9
Davi <davimesquita@gmail.com>
2015-06-11 13:20:41 -0300

    Update create.bat

--- create.bat
+++ create.bat
@@ -2,7 +2,7 @@ call cordova create polymer-tutorial-cordova org.polymer.cordova Polymer
 @cd polymer-tutorial-cordova
 echo ON config.xml, 'INDEX=' PUT THIS:: INDEX=finished/index.html >> config.xml
 @rmdir /S /Q www
-git clone -b 0.5.4-take2 https://github.com/Polymer/polymer-tutorial.git www
+git clone -b 1.0 https://github.com/Polymer/polymer-tutorial.git www
 @cd www
 @rmdir /S /Q .git
 @rmdir /S /Q starter
